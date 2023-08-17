a8a5f85e54353b78ad301b7450bd19a31222a176
Jennie Dalgren <jennie.dalgren@gmail.com>
2023-08-17 10:50:17 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,20 +1,20 @@
 // Start Here
 
-// Iteration 1 - Welcome and Introduction
+// Step 1 - Welcome and Introduction
 // Your Code Goes Here
 alert(
   `Welcome to our Javascript Pizzeria. 
 This little program will be executed without the necessity of using DOM Elements or functions in order to enhance your knowledge, Ready to Start? - Click 'OK' to begin.`
 );
 
-// Iteration 2 - Food Choice
+// Step 2 - Food Choice
 // Your Code Goes Here
 
-// Iteration 3 - Subtype Choice
+// Step 3 - Subtype Choice
 // Your Code Goes Here
 
-// Iteration 4 - Age
+// Step 4 - Age
 // Your Code Goes Here
 
-// Iteration 5 - Order Confirmation
+// Step 5 - Order Confirmation
 // Your Code Goes Here
