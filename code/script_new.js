7036db798f4d2438676b174725728e1080ff020f
Usha Lingamallu <lingamalluusha@gmail.com>
2023-10-01 14:11:33 +0200

    arrary1

--- code/script_new.js
+++ code/script_new.js
@@ -7,13 +7,29 @@ alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin
 let userInput = null
 let foodType = ["Pizza", "Pasta", "Salad"]
 let foodInput = 0
-let foodSelection = "Selection"
+let foodSelected = " "
 userInput = prompt("please enter your name:")
 
 alert(`Nice to meet you ${userInput}!`)
 foodInput=parseInt(prompt(`Select your food type: 1.${foodType[0]}, 2.${foodType[1]}, 3.${foodType[2]}`))
-foodSelection = foodType[0]
-alert(`You have selected ${foodSelection}`)
+switch(foodInput)
+{
+  case foodInput=1:
+    alert(`You have selected ${foodType[0]}`);
+    foodSelected=foodType[0];
+    break;
+  case foodInput=2:
+    alert(`You have selected ${foodType[1]}`);
+    foodSelected=foodType[1]
+    break;
+  case foodInput=3:
+    alert(`You have selected ${foodType[2]}`);
+    foodSelected=foodType[2]
+    break;
+  default:
+    alert(`Not entered available values`);
+}
+alert(`You have selected ${foodSelected}`)
 
 // Step 3 - Subtype choice
 // Your code goes here
